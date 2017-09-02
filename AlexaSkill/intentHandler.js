15cc8e89540c229ecd1a129a9b4ade4a191ee559
alasdair <alasdair@zeus>
2017-09-02 23:43:19 +0100

    Removed API Key style auth.

--- AlexaSkill/intentHandler.js
+++ AlexaSkill/intentHandler.js
@@ -2,8 +2,10 @@
 var intentHandler = {};
 var prompts = require('./prompts');
 var rp = require('request-promise');
+var config = require("./config.json");
+var fs = require("fs");
 
-intentHandler.launch = function(request, response) {
+intentHandler.launch = function( request, response ) {
   var prompt = prompts.launch.prompt;
   var reprompt = prompts.launch.reprompt;
 
@@ -17,12 +19,14 @@ intentHandler.ChannelChanger = function(req, res) {
       uri: "https://alasdair333.duckdns.org/alexa",
       method: "POST",
       headers: {'content-type' : 'application/x-www-form-urlencoded'},
-      form: {name: channelName,
-             key: "3"}
+      form: {name: channelName},
+      key: fs.readFileSync(config.key),
+      cert: fs.readFileSync(config.cert),
+      passphrase: config.passphrase
   };
 
   rp(options).then( function(rpresponse){
-      console.log(response);
+      console.log(rpresponse);
       res.say(rpresponse.body);
   });
 }
--- AlexaSkill/prompts.js
+++ AlexaSkill/prompts.js
@@ -2,8 +2,8 @@
 var prompts = {};
 
 prompts.launch = {
-  prompt: "Welcome to Who Is Right?  You can ask me a question like, who is right, is it Simon or Claire? ... Now, how can I help?",
-reprompt: "For instructions on what you can say, please say help me."
+  prompt: "Welcome, To Change the channel simple say. Alex, tell changechannel then the Channel Name",
+  reprompt: "For instructions on what you can say, please say help me."
 }
 
 prompts.channelchanger = {
