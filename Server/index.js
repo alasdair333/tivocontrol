58d59f2e9e5d61a05a849cde8eeff1711d24ff6f
alasdair <alasdair@zeus>
2017-09-02 23:56:39 +0100

    Updated for working with SSL Client keys

--- Server/index.js
+++ Server/index.js
@@ -3,14 +3,16 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 4000;
 
+var stringify = require('json-stringify');
+
 var fs = require("fs");
 var bodyParser = require('body-parser');
 var channelList = require('./channelList');
+var config = require('./config.json');
 var net = require('net');
-var API_KEY = 3;
 
-var tivoIP = "192.168.0.21";
-var tivoPort = "31339";
+//URL for Tivo documentation on TiVO commands:
+// https://www.tivo.com/assets/images/abouttivo/resources/downloads/brochures/TiVo_TCP_Network_Remote_Control_Protocol.pdf
 
 var client = new net.Socket();
 
@@ -19,36 +21,42 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.get('/', function(req, res){
+  res.send("OK Home");
+});
+
 app.get('/change', function(req,res){
-    res.send("OK");
+    res.send("Failed");
 });
 
 app.post('/change', function(req, res){
-  var name = req.body.name,
-      apiKey = req.body.key;
+  var validated = req.get('X-Validated').toString().trim().toLowerCase();
+  var issuer = req.get('X-Issuer');
 
-  console.log("ConnectedL Name: "+name);
-  if(API_KEY == apiKey) {
-      for(var c in channelList) {
-        if(channelList[c].name == name)
-        {
-          console.log("Found");
-          console.log("FORCECH "+ channelList[c].number);
-          client.write("FORCECH "+ channelList[c].number+"\r");
-          res.send("OK");
-          return;
-        }
+  if(validated === "success")
+  {
+    var name = req.body.name.trim();
+
+    for(var c in channelList) {
+      if(channelList[c].name.toLowerCase() == name.toLowerCase())
+      {
+        console.log("Found");
+        console.log("FORCECH "+ channelList[c].number);
+        client.write("FORCECH "+ channelList[c].number+"\r");
+        res.send("OK");
+        return;
       }
+    }
+
+    res.send("Not Found");
+    return;
   }
-  else {
-    console.log("Incorrect API Key");
-  }
-  res.send("Failed");
+  res.send("Failed\n");
 });
 
 app.listen(port, function(){
-  client.connect(tivoPort, tivoIP, function(){
-    console.log('CONNECTED TO: ' + tivoIP + ':' + tivoPort);
+  client.connect(config.tivo_port, config.tivo_ip, function(){
+    console.log('CONNECTED TO: ' + config.tivo_ip + ':' + config.tivo_port);
   });
   console.log("TiVO controller server started on port: "+port);
 
